4d66c0bf34ea5f1413c4f066145b887d290e331b
Pieter DG <pieter.de.geyndt@gmail.com>
2022-01-01 14:53:23 +0100

    v1.0 - Draft

--- who/models.py
+++ who/models.py
@@ -8,6 +8,10 @@ class Who(models.Model):
     whodob = models.DateField(verbose_name='Date of Birth',default=timezone.now)
     whobody = models.TextField(verbose_name = 'Who am I',default="")
     whocertifications = models.TextField(verbose_name = 'Cerftifications',default="")
+    whoemail = models.CharField(max_length = 200, verbose_name = 'e-mailaddress', default ="")
+    whoaddress = models.TextField(verbose_name='Address', default="")
+    whophone = models.CharField(max_length = 200, verbose_name='Tel nr', default="+32 (0) ")
+    whocv = models.FileField(verbose_name="Curriculum Vitae", upload_to="whomedia/wocv/",default="")
 
     def __str__(self):
         return self.whofirstname
